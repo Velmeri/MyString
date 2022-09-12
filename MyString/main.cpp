8d4af72aa1e1b13f8c0ea6cb3021d7031fba3b6e
Velmeri <danielmikhh2gmail.com>
2022-09-12 09:02:08 +0200

    добавление конструктор, который создаёт строку и инициализирует её строкой, полученной от пользователя

--- MyString/main.cpp
+++ MyString/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 class MyStr {
 	char* str;
 	int size;
 public:
 	MyStr();
+	MyStr(const char* str);
 };
 
 int main() {
@@ -16,4 +19,11 @@ MyStr::MyStr()
 {
 	size = 80;
 	str = new char[80];
+}
+
+MyStr::MyStr(const char* str)
+{
+	this->size = strlen(str);
+	this->str = new char[size];
+	std::strcpy(this->str, str);
 }
