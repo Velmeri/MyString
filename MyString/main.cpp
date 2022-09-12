c33ddf9c3368c342f8903bc11b2b140f1023e027
Velmeri <danielmikhh2gmail.com>
2022-09-12 08:41:41 +0200

    Создание конструктора по умолчанию

--- MyString/main.cpp
+++ MyString/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
-int main() {
+class MyStr {
+	char* str;
+	int size;
+public:
+	MyStr();
+};
 
+int main() {
+	
 	return 0;
+}
+
+MyStr::MyStr()
+{
+	size = 80;
+	str = new char[80];
 }
